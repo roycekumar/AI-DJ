7c078f2c10c0f3a5fc65cb31a1711c33d32917f1
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2020-11-06 16:24:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 scoreLeftWrist=0;
+scoreRightWrist=0;
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -20,7 +21,31 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("#FF0000");
+    if(scoreRightWrist>0.2){
+        circle(rightWristX,rightWristY,20);
+    
     console.log("scoreLeftWrist = "+scoreLeftWrist);
+    if(rightWristY>0 && rightWristY<=100){
+        document.getElementById("speed").innerHTML="Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY>100 && rightWristY<=200){
+        document.getElementById("speed").innerHTML="Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY>200 && rightWristY<=300){
+        document.getElementById("speed").innerHTML="Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300 && rightWristY<=400){
+        document.getElementById("speed").innerHTML="Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400 && rightWristY<=500){
+        document.getElementById("speed").innerHTML="Speed = 2.5x";
+        song.rate(2.5);
+    }
+}
     if(scoreLeftWrist>0.2){
         circle(leftWristX,leftWristY,20);
     InNumberleftWristY=Number(leftWristY);
@@ -53,6 +78,7 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         scoreLeftWrist=results[0].pose.keypoints[9].score;
+        scoreRightWrist=results[0].pose.keypoints[10].score;
         console.log("scoreLeftWrist = "+scoreLeftWrist);
         console.log(results);
         leftWristX=results[0].pose.leftWrist.x;
